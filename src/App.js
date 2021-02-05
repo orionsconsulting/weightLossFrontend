f0c6dcc9f74a75e035e905506bc797168f32ae3d
Jennifer Leih Williams <jenniferleihwilliams@Jennifers-MacBook-Pro.local>
2021-02-05 09:23:57 -0600

    made header more independent

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 import React,{useState,useEffect} from 'react';
 import './App.css';
-import Header from "./components/header"
+import Header from "./components/Header"
 
 function App() {
-  const [data,setData]=useState([]);
-  const getData=()=>{
-    fetch('user.json'
-    ,{
-      headers : { 
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       }
-    }
-    )
-      .then(function(response){
-        console.log(response)
-        return response.json();
-      })
-      .then(function(myJson) {
-        console.log(myJson);
-        setData(myJson)
-      });
-  }
-  useEffect(()=>{
-    getData()
-  },[])
+  
   return (
     <div className="App">
-       <Header userData={data}/>
+       <Header />
     </div>
   );
 }
