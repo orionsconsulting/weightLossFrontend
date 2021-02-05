8e9d0ed7194bf972f428ccf77f482562a61c7407
Jennifer Leih Williams <jenniferleihwilliams@Jennifers-MacBook-Pro.local>
2021-02-05 09:41:03 -0600

    added tracking div

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React,{useState,useEffect} from 'react';
 import './App.css';
-import Header from "./components/Header"
+import Header from "./components/Header";
+import Card  from "./containers/Card";
 
 function App() {
   
   return (
     <div className="App">
        <Header />
+       <Card />
     </div>
   );
 }
