817c7991adaefde193265a9bc8200297c1370855
Jennifer Leih Williams <jenniferleihwilliams@Jennifers-MacBook-Pro.local>
2021-02-07 10:57:39 -0600

    adding code for automatic deployment

---
+++ src/Header.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userData: [],
+      loading: true,
+    };
+  }
+
+  async componentDidMount() {
+    const user = await fetch('./user.json');
+    const userJson = await user.json();
+    
+    if (userJson) {
+        "date"(userJson);
+      this.setState({
+        userData: userJson,
+        loading: false,
+      });
+    }
+  }
+
+  render() {
+    const { userData, loading } = this.state;
+
+    if (loading) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <div className='row'>
+          <header>
+        <p>
+            Welcome to the Weight Loss Tracker, {userData.user}
+        </p>
+        <p>Your goal is {userData.goal}</p>
+    </header>
+      </div>
+    );
+  }
+}
+
+export default Header;
