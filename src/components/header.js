6e506e1a08fc2614c07331e400f31d6586eeb469
Jennifer Leih Williams <jenniferleihwilliams@Jennifers-MacBook-Pro.local>
2021-02-07 10:54:12 -0600

    Added package lock

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,45 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 
-const Header = ({userData}) => (
+class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userData: [],
+      loading: true,
+    };
+  }
+
+  async componentDidMount() {
+    const user = await fetch('./user.json');
+    const userJson = await user.json();
     
-    <header>
+    if (userJson) {
+        console.log(userJson);
+      this.setState({
+        userData: userJson,
+        loading: false,
+      });
+    }
+  }
+
+  render() {
+    const { userData, loading } = this.state;
+
+    if (loading) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <div className='row'>
+          <header>
         <p>
             Welcome to the Weight Loss Tracker, {userData.user}
         </p>
         <p>Your goal is {userData.goal}</p>
     </header>
-);
+      </div>
+    );
+  }
+}
 
 export default Header;
